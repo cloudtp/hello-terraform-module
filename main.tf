ab2ec1da25e561f67df6785adc9b113b1366b12c
Steph <ospina@hpe.com>
2020-01-31 12:47:54 -0500

    add outputs for main

--- main.tf
+++ main.tf
@@ -5,11 +5,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "aws_ami_id" {
+module "aws_ami" {
   source = "./modules/ami_module"
 }
 
-module "aws_ec2_id" {
-  source = "./modules/ec2_module"
-  aws_ami_id = module.aws_ami_id.aws_ami_id
-}
+# module "aws_ec2" {
+#   source = "./modules/ec2_module"
+#   aws_ami_id = module.aws_ami.aws_ami_id
+# }
+
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "aws_ami_id" {
+  value = module.aws_ami.aws_ami_id
+}
